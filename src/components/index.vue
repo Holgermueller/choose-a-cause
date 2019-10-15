b9959df706b767ba4740260cc6e10a26de4d6f3f
Holger Mueller <9jholger@gmail.com>
2019-10-15 00:06:46 -0500

    got api working

--- src/components/index.vue
+++ src/components/index.vue
@@ -21,10 +21,18 @@ export default {
     findCharity() {
       const apiKey = process.env.VUE_APP_CHARITY_API;
       const applicationID = process.env.VUE_APP_APID;
+      console.log(apiKey, applicationID)
 
-      axios.get("https://api.data.charitynavigator.org/v2").then(response => {
-        console.log(response);
-      });
+      axios
+        .get(
+          "https://api.data.charitynavigator.org/v2/Organizations?app_id=" +
+            applicationID +
+            "&app_key=" +
+            apiKey
+        )
+        .then(response => {
+          console.log(response.data[0]);
+        });
     }
   }
 };

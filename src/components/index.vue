65bd79adbea4020c896e9be96c4379a3ccbd8828
Holger Mueller <9jholger@gmail.com>
2019-10-15 21:27:47 -0500

    put card in own file

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,40 +1,49 @@
 <template>
   <div>
-    <h1>Hello</h1>
-
-    <v-card>
-      <v-btn @click="findCharity">find a charity</v-btn>
-    </v-card>
+    <!-- <v-card>
+      <v-btn>find a charity</v-btn>
+    </v-card>-->
+    <CharityCard :charities_array="charities_array" />
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import CharityCard from "../components/layout/CharityCard";
 
 export default {
+  components: {
+    CharityCard
+  },
   data() {
     return {
-      charities: []
+      charities_array: []
     };
   },
-  methods: {
-    findCharity() {
-      const apiKey = process.env.VUE_APP_CHARITY_API;
-      const applicationID = process.env.VUE_APP_APID;
-      console.log(apiKey, applicationID)
+  mounted() {
+    const apiKey = process.env.VUE_APP_CHARITY_API;
+    const applicationID = process.env.VUE_APP_APID;
 
-      axios
-        .get(
-          "https://api.data.charitynavigator.org/v2/Organizations?app_id=" +
-            applicationID +
-            "&app_key=" +
-            apiKey
-        )
-        .then(response => {
-          console.log(response.data[0]);
+    axios
+      .get(
+        "https://api.data.charitynavigator.org/v2/Organizations?app_id=" +
+          applicationID +
+          "&app_key=" +
+          apiKey
+      )
+      .then(res => {
+        let allCharities = res.data;
+        allCharities.map(charity => {
+          this.charities_array.push(charity);
         });
-    }
-  }
+        console.log(this.charities_array[0]);
+      })
+      .catch(err => {
+        console.log("Error:" + err);
+      });
+  },
+
+  methods: {}
 };
 </script>
 
---
+++ src/components/layout/CharityCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id>
+    <v-hover v-for="charity in charities_array" :key="charity.ein">
+      <v-card class="info-card" tile>
+        <h1>{{charity.charityName}}</h1>
+        <hr />
+        <p>{{charity.mailingAddress.city}}</p>
+        <p>{{charity.mailingAddress.stateOrProvince}}</p>
+        <v-card-actions>
+          <v-btn :id="charity.ein" @click="learnMore">Learn more</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-hover>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    charities_array: {
+      type: Array,
+      required: true
+    },
+    methods: {
+      learnMore() {
+        let targetId = event.currentTarget.id;
+        console.log("targetId");
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.info-card {
+  width: 85%;
+  margin: 2% auto;
+}
+</style>

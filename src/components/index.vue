5e542da7aeddcf29a072c6fe8d01ef62d3d1ad50
Holger Mueller <9jholger@gmail.com>
2019-10-16 21:27:49 -0500

    test push

--- src/components/index.vue
+++ src/components/index.vue
@@ -13,30 +13,11 @@ export default {
   components: {},
   data() {
     return {
-      charities_array: []
+      
     };
   },
   mounted() {
-    const apiKey = process.env.VUE_APP_CHARITY_API;
-    const applicationID = process.env.VUE_APP_APID;
 
-    axios
-      .get(
-        "https://api.data.charitynavigator.org/v2/Organizations?app_id=" +
-          applicationID +
-          "&app_key=" +
-          apiKey
-      )
-      .then(res => {
-        let allCharities = res.data;
-        allCharities.map(charity => {
-          this.charities_array.push(charity);
-        });
-        console.log(this.charities_array[0]);
-      })
-      .catch(err => {
-        console.log("Error:" + err);
-      });
   },
 
   methods: {}

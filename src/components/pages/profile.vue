c13d04675768da80558295484426669275d9d064
Holger Mueller <9jholger@gmail.com>
2019-10-17 21:27:07 -0500

    worked on firebase

--- src/components/pages/profile.vue
+++ src/components/pages/profile.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="profile">
-
-    <AddClassDialog />
+    <AddClassDialog :classList="classList" />
 
     <h1>Classes will appear below.</h1>
-    
-  <ClassCard 
-  v-for="single_class in classList" :key="single_class.id"/>
 
+    <ClassCard v-for="single_class in classList" :key="single_class.id" />
   </div>
 </template>
 
 <script>
-import ClassCard from "../layout/ClassCard"
-import AddClassDialog from "../../forms/addAClass"
+import db from "../firebase/firebaseInit";
+import ClassCard from "../layout/ClassCard";
+import AddClassDialog from "../../forms/addAClass";
 
 export default {
   name: "Profile",
@@ -22,13 +20,27 @@ export default {
     AddClassDialog
   },
   data() {
-    return{
-      classList: [],
-    }
+    return {
+      classList: []
+    };
   },
-  methods: {
+  created() {
+    db.collection("classes")
+      .get()
+      .then(querySnapshot => {
+        console.log(querySnapshot);
+        // querySnapshot.forEach(doc => {
+        //   console.log(doc);
+        //   // const data = {
 
-  }
+        //   // }
+        // });
+      })
+      .catch(err => {
+        console.log("Error getting document: " + err);
+      });
+  },
+  methods: {}
 };
 </script>
 
--- src/forms/addAClass.vue
+++ src/forms/addAClass.vue
@@ -8,12 +8,29 @@
       <v-card>
         <v-card-title>Add a class:</v-card-title>
         <v-card-text>
-          <v-text-field></v-text-field>
+          <v-container fluid>
+            <v-layout wrap>
+              <v-flex xs12 sm12 md13 lg12 xl12>
+                <v-text-field
+                  type="text"
+                  v-model="class_name"
+                  label="Enter class name here*"
+                  hint="Ex.: Multicultural Lit T-Th 9-9:50"
+                  persistent-hint
+                  prepend-icon="mdi-account-group"
+                  append-icon="mdi-close"
+                  ref="textField"
+                  @click:append="clearField"
+                ></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
         </v-card-text>
         <v-card-actions>
-          <v-spacer></v-spacer>
+          <
           <v-btn color="red" @click="dialog = false">Cancel</v-btn>
-          <v-btn color="blue">Submit</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="blue" @click="addClass">Submit</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -28,6 +45,27 @@ export default {
       errors: [],
       class_name: null
     };
+  },
+  props: {
+    classList: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    addClass() {
+      let class_name = this.class_name;
+      console.log(class_name);
+      this.clearField();
+      this.dialog = false;
+    },
+    updateClassList() {
+      let classList = this.classList;
+      classList.length = 0;
+    },
+    clearField() {
+      this.$refs.textField.reset();
+    }
   }
 };
 </script>

97b1a1137692998664c8bf8b8bf64c4249651d07
Holger Mueller <9jholger@gmail.com>
2019-12-01 13:55:36 -0600

    began alert component

---
+++ src/components/shared/alert.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <v-alert type="error" dark dismissible tile @input="onClose">
+      alert!!
+    </v-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      alert: true
+    };
+  },
+  methods: {
+    onClose() {
+      this.$emit("dismissed");
+    }
+  }
+};
+</script>
+
+<style scoped></style>
